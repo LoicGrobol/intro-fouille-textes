5f8a91294086da12fa65c3e1aa1f98888dd04fdd
Loïc Grobol <loic.grobol@gmail.com>
2018-03-05 18:56:29 +0100

    add: CLI for vectorisation.py

--- tools/vectorisation.py
+++ tools/vectorisation.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import argparse
 import pathlib
 import re
 import sys
@@ -30,23 +31,22 @@ def vocabulaire(liste_fichiers, mots_vides):
     return sorted(vocab)
 
 
-def construire_vecteur(voc_ref, mots_fichier, representation):
+def construire_vecteur(voc_ref, mots_fichier, boolean):
     this_voc = Counter(mots_fichier)
     vecteur = [this_voc.get(m, 0) for m in voc_ref]
-    if representation == 1:
+    if boolean:
         vecteur = [1 if v else 0 for v in vecteur]
     return vecteur
 
 
-def main_entry_point(argv=sys.argv):
-    # programme principal
-    # demande de saisie du nom du dossier principal et du fichier de mots vides
-    dossier = pathlib.Path(input("Nom du dossier contenant le corpus : "))
-    fichier_mots_vides = input("Nom du fichier de mots vides (se terminant par .txt) : ")
-    liste_mots_vides = lecture(fichier_mots_vides)
-    choix = None
-    while (choix not in ('1', '2')):
-        choix = input("représentation booléenne (taper 1) ou en nombre d\'occurrences (taper 2) ? ")
+def process(corpus_path, out_path=None, boolean=False, fichier_mots_vides=None):
+    dossier = pathlib.Path(corpus_path)
+    if out_path is None:
+        out_path = dossier.parent/'fichier-resultat.arff'
+    if fichier_mots_vides is None:
+        liste_mots_vides = []
+    else:
+        liste_mots_vides = lecture(fichier_mots_vides)
     # chaque sous-dossier du dossier principal est une etiquette de classe
     etiquettes = [d.name for d in dossier.iterdir() if d.is_dir]
     v = vocabulaire(pathlib.Path(dossier).glob('*/*.txt'), liste_mots_vides)
@@ -59,16 +59,44 @@ def main_entry_point(argv=sys.argv):
         liste = (dossier/nom).glob("*.txt")
         for fichier in liste:
             liste_mots = lecture(fichier)
-            vecteur = construire_vecteur(v, liste_mots, choix)
+            vecteur = construire_vecteur(v, liste_mots, boolean)
             vecteur.append(nom)
             sortie.append(','.join(map(str, vecteur)))
 
     # ecriture du contenu de la variable dans le fichier de sortie
-    with open(dossier.parent/'fichier-resultat.arff', 'w') as fichier_sortie:
+    with open(out_path, 'w') as fichier_sortie:
         fichier_sortie.write('\n'.join(sortie))
 
     print('fichier-resultat.arff produit !')
 
 
+def main_entry_point(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+    if argv:
+        parser = argparse.ArgumentParser(description='Process some integers.')
+        parser.add_argument('corpus_path', metavar='dossier_corpus', type=str,
+                            help='Le dossier contenant corpus (un sous-dossier par étiquette)')
+        parser.add_argument('out_path', metavar='fichier_sortie',
+                            nargs='?', type=str, default=None,
+                            help='Le dossier contenant corpus (un sous-dossier par étiquette)')
+        parser.add_argument('--mots-vides', metavar='fichier_mots_vides', type=str, default=None,
+                            help='Un fichier contenant une liste de mots vides (un par ligne)')
+        parser.add_argument('--boolean', action='store_true',
+                            help='Utiliser des représentations booléennes')
+
+        args = parser.parse_args(argv)
+        return process(args.corpus_path, args.out_path, args.boolean, args.mots_vides)
+
+    # Legacy interactive mode
+    corpus_path = input("Nom du dossier contenant le corpus : ")
+    fichier_mots_vides = input("Nom du fichier de mots vides (se terminant par .txt) : ")
+
+    choix = None
+    while choix not in ('1', '2'):
+        choix = input("représentation booléenne (taper 1) ou en nombre d\'occurrences (taper 2) ? ")
+    process(corpus_path, boolean=choix == 1, fichier_mots_vides=fichier_mots_vides)
+
+
 if __name__ == '__main__':
-    sys.exit(main_entry_point(sys.argv))
+    sys.exit(main_entry_point(sys.argv[1:]))
